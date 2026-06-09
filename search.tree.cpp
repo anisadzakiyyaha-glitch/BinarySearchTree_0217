06bf8a96dec8b6ac225b44c9054c8866596f619a
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-06-09 11:21:05 +0700

    tambah pilihan menu

--- search.tree.cpp
+++ search.tree.cpp
@@ -142,6 +142,44 @@ int main()
             cin >> ch;
 
             cout << endl;
-       }
 
+            switch (ch)
+            {
+            case '1':
+            {
+                cout << "enter a word: ";
+                string word;
+                cin >> word;
+                obj.insert(word);
+                break;
+            }
+            case '2':
+            {
+                obj.inorder(obj.root);
+                break;
+            }
+
+            case '3':
+            {
+                obj.preorder(obj.root);
+                break;
+            }
+
+            case '4':
+            {
+                obj.postorder(obj.root);
+                break;
+            }
+
+            case '5':
+                return 0;
+            
+            default:
+            {
+                cout << "invalid option" << endl;
+                break;
+            }
+        }
+    }
+    return 0;
     }
